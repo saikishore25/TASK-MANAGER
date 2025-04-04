d75ae48d8e2b17527e6e3b353b627aaefa292e1b
Kishore <makam.kishore.991.sandhya@gmail.com>
2025-04-04 15:47:49 +0530

    main commit

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -20,7 +20,9 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", 
+      "https://task-manager-frontend-orcin-delta.vercel.app"
+    ],
     credentials:true
 }))
 
